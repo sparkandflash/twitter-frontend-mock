eb7deadec72b8bceeb591d5485b733cedff72263
plaaz <phegde04@gmail.com>
2022-12-16 16:40:30 +0000

    a

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,30 @@
-import {VStack, Text, Input, Box} from '@chakra-ui/react'
-
+import { VStack, Text, Input, InputGroup, InputLeftElement, Box } from '@chakra-ui/react'
+import { Search2Icon } from '@chakra-ui/icons'
 function Search() {
     return (
-        
-   
-        <VStack  paddingTop={1} paddingInline={5} w='400px' borderLeftWidth='1px' minH={'100%'} >
-        <Input w='270px' borderColor='rgb(247, 249, 249)' bgColor='rgb(235, 237, 237)' borderRadius='20px' placeholder='search twitter' />
-        <Box w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)'  borderWidth='1px'p='20px' borderRadius='lg' h='300px'> <Text as='b'>what 's happening</Text></Box>
-        
-        <Box w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' p='20px' borderRadius='lg' h='300px'>  <Text as='b'>who to follow</Text> </Box>
-      
+
+
+        <VStack paddingTop='20px' paddingRight='80px' paddingLeft='20px' w='400px' borderLeftWidth='1px' minH={'100%'} >
+
+            <InputGroup marginLeft= '30px'>
+                <InputLeftElement
+
+                    bgColor='rgb(235, 237, 237)'
+                    pointerEvents='none'
+                    borderRadius='20px'
+                    children={<Search2Icon bgColor='rgb(235, 237, 237)' color='gray.500' />}
+                />
+                <Input borderColor='rgb(247, 249, 249)' w='270px' borderRadius='20px' bgColor='rgb(235, 237, 237)' placeholder='search twitter' />
+            </InputGroup>
+
+            <Box   w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' p='20px' borderRadius='lg' h='300px'> <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>what 's happening</Text></Box>
+
+            <Box   w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' p='20px' borderRadius='lg' h='300px'>  <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>Who to follow</Text> </Box>
+
         </VStack>
-       
-        
+
+
     );
-  }
-  
-  export default Search;
+}
+
+export default Search;
