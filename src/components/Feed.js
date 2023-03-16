95ff7cedd5619d28e337176e5218491e8a5b3282
plaaz <phegde04@gmail.com>
2023-03-16 08:04:33 +0000

    aa

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,5 @@
 //infinite scroll
-import {  HStack, Image, Spacer,  Text, Textarea, Box, Button, Flex } from '@chakra-ui/react'
+import { HStack, Image, Spacer, Tab, TabList, Tabs, Text, Textarea, Box, Button, Flex } from '@chakra-ui/react'
 import TwiPost from './TwiPost';
 function Feed() {
     return (
@@ -9,23 +9,33 @@ function Feed() {
                 <Text fontSize='lg' className='menu-home' as='b'>Home</Text>
             </Box>
 
-            <HStack> <Box w="50%" className='tab-selection' p='4'> <Text fontFamily='chirp-medium-web' className='tab-selection-text' align="center" >For You</Text> </Box>  <Box className='tab-selection' w='50%' p='4'> <Text className='tab-selection-text' fontFamily='chirp-medium-web' align="center" >following </Text> </Box> </HStack>
-
-            <Box paddingLeft='15px' paddingRight="15px" h='200px' borderTopWidth='1px'  borderBottomWidth='1px' minW={'100%'}>
-                <HStack height="130px">
+            {/*  <HStack> <Box w="50%" className='tab-selection' p='4'> <Text fontFamily='chirp-medium-web' className='tab-selection-text' align="center" >For You</Text> </Box>  <Box className='tab-selection' w='50%' p='4'> <Text className='tab-selection-text' fontFamily='chirp-medium-web' align="center" >following </Text> </Box> </HStack> */}
+            <Tabs colorScheme={'twitter'} >
+                <TabList>
+                    <Box w="50%" align="center" className='tab-selection' >
+                        <Tab ><Text fontFamily='chirp-bold-web' className='tab-selection-text' align="center" >For You</Text></Tab>
+                    </Box>
+                    <Box w="50%" align="center" className='tab-selection'>
+                        <Tab ><Text className='tab-selection-text' fontFamily='chirp-bold-web' align="center" >following </Text> </Tab>
+                    </Box>
+                </TabList>
+            </Tabs>
+            <Box className='home-tweet-box' borderBottomWidth='1px' >
+                <Box display='flex' >
                     <Image
-                        marginBottom='42px'
+                        marginBottom='50px'
                         borderRadius='full'
                         boxSize='48px'
                         src='https://bit.ly/dan-abramov'
                         alt='Dan Abramov'
                     />
-                    <Textarea resize="none" fontSize={19}  variant='unstyled' className='place-holder-textarea' placeholder="What's happening ?" />
+                    <Spacer padding={1}/>
+                    <Textarea resize="none"  fontSize={19} variant='unstyled' className='place-holder-textarea' placeholder="What's happening ?" />
+                </Box>
+                <Box display='flex' >
+                    <Button></Button>  <Spacer /> <Button disabled='true' borderRadius={50} colorScheme={'twitter'}>Tweet</Button>
+                </Box>
 
-                </HStack>
-
-                <HStack>  <Button></Button>  <Spacer /> <Button disabled='true' borderRadius={50} colorScheme={'twitter'}>Tweet</Button>
-                </HStack>
             </Box>
 
             <Box >  <TwiPost /> </Box>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
-import { VStack, Text, Input, InputGroup, InputLeftElement, Box } from '@chakra-ui/react'
+import { VStack, Text, Input, InputGroup, Spacer, InputLeftElement, Box } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
 function Search() {
     return (
 
 
-        <VStack paddingTop='20px' paddingRight='80px' paddingLeft='20px' w='400px' borderLeftWidth='1px' minH={'100%'} >
+        <VStack paddingTop='20px' paddingRight='80px' paddingLeft='20px' w='500px' borderLeftWidth='1px' minH={'100%'} >
 
             <InputGroup marginLeft= '30px'>
                 <InputLeftElement
@@ -14,12 +14,12 @@ function Search() {
                     borderRadius='20px'
                     children={<Search2Icon bgColor='rgb(235, 237, 237)' color='gray.500' />}
                 />
-                <Input borderColor='rgb(247, 249, 249)' w='270px' borderRadius='20px' bgColor='rgb(235, 237, 237)' placeholder='search twitter' />
+                <Input borderColor='rgb(247, 249, 249)' w='370px' borderRadius='20px' bgColor='rgb(235, 237, 237)' font-family='chirp-regular-web' placeholder='Search Twitter' />
             </InputGroup>
 
-            <Box   w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' paddingLeft='20px' paddingTop='10px' borderRadius='lg' h='300px'> <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>what 's happening</Text></Box>
+            <Box   w='370px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' paddingLeft='20px' paddingTop='10px' borderRadius='lg' h='300px'> <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>what 's happening</Text></Box>
 
-            <Box   w='270px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' p='20px' borderRadius='lg' h='300px'>  <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>Who to follow</Text> </Box>
+            <Box   w='370px' bgColor='rgb(247, 249, 249)' borderColor='rgb(247, 249, 249)' borderWidth='1px' p='20px' borderRadius='lg' h='300px'>  <Text  bgColor='rgb(247, 249, 249)' fontSize='xl' as='b'>Who to follow</Text> </Box>
 
         </VStack>
 
